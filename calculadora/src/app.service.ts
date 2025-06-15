90efc882a85f5d91709f454e2836e88e284e383d
Joel David Ximenez Calderon <joel.ximenez@epn.edu.ec>
2025-06-14 20:18:13 -0500

    servicio

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppService { 
   getHello(): string {
-    //console.log('log!');
-    //console.debug('debug!');
-    //console.info('info!');
     console.warn('warn!');
     console.error('error!');
-    return 'Hello World!';
+    return 'Hello World!!!';
   }
 }
+
